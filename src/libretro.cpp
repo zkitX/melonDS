f8e452478d54509bc6a6f9a4578c3f34cc37516b
twinaphex <libretro@gmail.com>
2017-04-24 20:43:16 +0200

    Update to 0.2

--- src/libretro.cpp
+++ src/libretro.cpp
@@ -68,7 +68,7 @@ void retro_get_system_info(struct retro_system_info *info)
 {
    memset(info, 0, sizeof(*info));
    info->library_name     = "melonDS";
-   info->library_version  = "0.1";
+   info->library_version  = "0.2";
    info->need_fullpath    = true;
    info->valid_extensions = "nds";
 }
