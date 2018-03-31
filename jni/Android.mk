9226c2d163f80c9d32525700c1032869f5fa9b7f
Aaron Kling <webgeek1234@gmail.com>
2018-03-31 12:13:35 -0500

    Fix up android jni makefiles

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,29 +1,17 @@
 LOCAL_PATH := $(call my-dir)
 
+CORE_FLAGS :=
+MELON_DIR  := ../src
+CORE_DIR   := $(MELON_DIR)/libretro
+
+include ../Makefile.common
+
+CORE_FLAGS += -D__LIBRETRO__ $(INCFLAGS)
+
 include $(CLEAR_VARS)
-
 LOCAL_MODULE    := retro
-
-ifeq ($(TARGET_ARCH),arm)
-LOCAL_CFLAGS += -DANDROID_ARM
-LOCAL_ARM_MODE := arm
-endif
-
-ifeq ($(TARGET_ARCH),x86)
-LOCAL_CFLAGS +=  -DANDROID_X86
-endif
-
-ifeq ($(TARGET_ARCH),mips)
-LOCAL_CFLAGS += -DANDROID_MIPS -D__mips__ -D__MIPSEL__
-endif
-
-CORE_DIR	= ..
-
-include $(CORE_DIR)/Makefile.common
-
 LOCAL_SRC_FILES    += $(SOURCES_C) $(SOURCES_CXX)
-LOCAL_CFLAGS += -O2 -D__LIBRETRO__ -DNDEBUG=1 $(INCFLAGS)
-
-#LOCAL_LDLIBS += -lz
+LOCAL_CFLAGS += $(CORE_FLAGS)
+LOCAL_CPPFLAGS += -std=c++11 $(CORE_FLAGS)
 
 include $(BUILD_SHARED_LIBRARY)
--- jni/Application.mk
+++ jni/Application.mk
@@ -1,1 +1,2 @@
-APP_ABI := all
+APP_ABI := armeabi armeabi-v7a arm64-v8a x86 x86_64
+APP_STL := gnustl_static
